d235b5c3107068f0b72b747784a911c587114e58
Jay <jay.sanjay.karia@gmail.com>
2025-02-11 16:19:34 +0000

    feat: add apis for setting and clearing data

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,28 @@ export class JQLite {
   public configManager: ConfigManager;
   public data: string | object;
 
+  /**
+   * The constructor for JQLite
+   * @param config The config object to override
+   * @param data The JSON data or path to a JSON file
+   */
   constructor(config?: Config, data?: string) {
     this.configManager = new ConfigManager(config);
     this.data = data ? validateData(data as string) : {};
+  }
+
+  /**
+   * Overwrites the data
+   * @param data The data to overwrite
+   */
+  public setData(data: string): void {
+    this.data = validateData(data);
+  }
+
+  /**
+   * Clears the data
+   */
+  public clearData(): void {
+    this.data = {};
   }
 }
--- tests/data.test.ts
+++ tests/data.test.ts
@@ -19,7 +19,7 @@ describe("Data Validation while init", () => {
     }).toThrowError();
   });
 
-  test("should not accpet invalid JSON file path", () => {
+  test("should not accept invalid JSON file path", () => {
     let jqlite: JQLite;
 
     expect(() => {
@@ -54,5 +54,101 @@ describe("Data Validation while init", () => {
         hello: "world",
       })
     );
+  });
+});
+
+/**
+ * Set Data
+ */
+describe("Set Data", () => {
+  test("should set the data", () => {
+    const jqlite = new JQLite();
+    jqlite.setData(JSON.stringify({ key: "value" }));
+    expect(jqlite.data).toEqual(
+      JSON.stringify({
+        key: "value",
+      })
+    );
+  });
+
+  test("should not accept invalid JSON data", () => {
+    const jqlite = new JQLite();
+    expect(() => {
+      jqlite.setData("invalid-json");
+      expect(jqlite.data).toEqual({});
+    }).toThrowError();
+  });
+
+  test("should not accpet invalid JSON file path", () => {
+    const jqlite = new JQLite();
+    expect(() => {
+      jqlite.setData("invalid-path");
+      expect(jqlite.data).toEqual({});
+    }).toThrowError();
+
+    expect(() => {
+      jqlite.setData("./src/index.ts");
+      expect(jqlite.data).toEqual({});
+    }).toThrowError();
+  });
+
+  test("should accept valid JSON data", () => {
+    const jqlite = new JQLite();
+    jqlite.setData(
+      JSON.stringify({
+        key: "value",
+      })
+    );
+    expect(jqlite.data).toEqual(
+      JSON.stringify({
+        key: "value",
+      })
+    );
+  });
+
+  test("should accept valid JSON file path", () => {
+    const jqlite = new JQLite();
+    jqlite.setData("./tests/data.json");
+    expect(jqlite.data).toEqual(
+      JSON.stringify({
+        hello: "world",
+      })
+    );
+  });
+
+  test("should overwrite the existing data", () => {
+    const jqlite = new JQLite({}, JSON.stringify({ key: "value" }));
+    jqlite.setData(
+      JSON.stringify({
+        hello: "world",
+      })
+    );
+    expect(jqlite.data).toEqual(
+      JSON.stringify({
+        hello: "world",
+      })
+    );
+  });
+});
+
+/**
+ * Clear Data
+ */
+describe("Clear Data", () => {
+  test("should clear the data", () => {
+    const jqlite = new JQLite();
+    jqlite.setData(
+      JSON.stringify({
+        key: "value",
+      })
+    );
+    jqlite.clearData();
+    expect(jqlite.data).toEqual({});
+  });
+
+  test("should clear the data if not set", () => {
+    const jqlite = new JQLite();
+    jqlite.clearData();
+    expect(jqlite.data).toEqual({});
   });
 });
