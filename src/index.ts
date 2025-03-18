2def563d068a07c53c73d7769df109faf25ff355
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-03-18 10:46:33 +0530

    fix: import and exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { queryRunner } from "./core/queryRunner";
-import { cacheManager } from "./cache/cacheManager";
-import { configManager } from "./config/configManager";
-import { eventManager } from "./events/eventManager";
 
-export { queryRunner, cacheManager, configManager, eventManager };
+export { queryRunner };
 
 export default {
   queryRunner,
-  cacheManager,
-  configManager,
-  eventManager,
 };
