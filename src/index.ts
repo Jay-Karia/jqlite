390236de3f8c5a37e72434dd3c97e2f4675875fc
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-03-21 13:00:32 +0530

    chore: remove JQLite main class
    and remove exports for DataManager, ConfigManager and QueryRunner class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import { queryRunner, QueryRunner } from "./core/runner";
-import { dataManager, DataManager } from "./data/manager";
-import { configManager, ConfigManager } from "./config/manager";
-import { JQLiteInstances } from "types/index";
+import { queryRunner } from "./core/runner";
+import { dataManager } from "./data/manager";
+import { configManager } from "./config/manager";
 
-export class JQLite {
-  public queryRunner = queryRunner;
-  public dataManager = dataManager;
-  public configManager = configManager;
-
-  constructor(options?: JQLiteInstances) {
-    if (options?.queryRunner) this.queryRunner = queryRunner;
-    if (options?.dataManager) this.dataManager = dataManager;
-    if (options?.configManager) this.configManager = configManager;
-  }
-}
-
-export { queryRunner, dataManager, configManager, QueryRunner, DataManager, ConfigManager };
+export { queryRunner, dataManager, configManager };
