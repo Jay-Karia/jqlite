7b0963c9fef0fb84971c45aaf1fa3491cad7a534
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-03-21 13:54:21 +0530

    refactor: remove unused code from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,4 @@ import { queryRunner } from "./core/runner";
 import { dataManager } from "./data/manager";
 import { configManager } from "./config/manager";
 
-// configManager.set({ defaultPath: "../test.json" });
-dataManager.set({hello: "world1"});
-
-dataManager.save();
-
 export { queryRunner, dataManager, configManager };
