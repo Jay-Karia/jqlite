b9e801db91b2b2b4b99d61a7247d381e8f13d5b7
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-03-24 18:05:18 +0530

    feat: implement can stream file function in data streamer

--- src/data/streamer.ts
+++ src/data/streamer.ts
@@ -1,6 +1,55 @@
+import {loadDefaultConfig} from "config/loader";
+import {configStore} from "config/store";
+import {DataError} from "errors/factory";
+import {ERROR_MESSAGES} from "errors/messages";
+import {existsSync, statfsSync} from "fs";
+
 /**
  * DataStreamer Class
  */
-export class DataStreamer {}
+export class DataStreamer {
+  private _buffer: Buffer;
+  private _chunk: Buffer;
+
+  private _chunkSize: number;
+  private _bufferSize: number;
+  private _minDataSize: number;
+
+  private _defaultConfig = loadDefaultConfig();
+
+  constructor() {
+    // Initialize the buffer and chunk
+    this._buffer = Buffer.alloc(0);
+    this._chunk = Buffer.alloc(0);
+
+    // Initialize the default chunk size, buffer size and min data size
+    this._chunkSize = this._defaultConfig.dataStreaming.chunkSize;
+    this._bufferSize = this._defaultConfig.dataStreaming.bufferSize;
+    this._minDataSize = this._defaultConfig.dataStreaming.minDataSize;
+  }
+
+  /**
+   * Check whether the file can be streamed
+   * @param {string} filePath The path to the file
+   * @description This method checks if the file can be streamed based on its size.
+   * @returns {boolean} True if the file can be streamed, false otherwise
+   */
+  public canStreamFile(filePath: string): boolean {
+    // Get the minimum data size from the config
+    this._minDataSize = configStore.get().dataStreaming?.minDataSize || this._defaultConfig.dataStreaming.minDataSize;
+
+    // Check if the file exists
+    const isFile = existsSync(filePath);
+    if (!isFile) throw new DataError(ERROR_MESSAGES.DATA.INVALID_FILE_PATH, {
+      filePath,
+      isFile
+    });
+
+    // Get the file size
+    const fileSize = statfsSync(filePath).bsize;
+    return fileSize > this._minDataSize;
+  }
+
+}
 
 export const dataStreamer = new DataStreamer();
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { queryRunner } from "./core/runner";
 import { dataManager } from "./data/manager";
 import { configManager } from "./config/manager";
 
-// TODO: add data streaming
+// TODO: fix config store get function to return the guaranteed config object
 // TODO: fix: add type validation in json configs (later)
 
 export { queryRunner, dataManager, configManager };
